a57ece937f5a8e6e8bd57aa78e173f67b4dc0c3b
Eric Ma <officialpassawis@gmail.com>
2021-03-15 00:59:09 +0700

    able to get oldest, still need to fix case for initialising new date each time

--- Coursework2/src/Main.java
+++ Coursework2/src/Main.java
@@ -1,10 +1,37 @@
 import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(final String[] args)
     {
-        SwingUtilities.invokeLater(GUI::new);
+//        SwingUtilities.invokeLater(GUI::new);
+            Model model = new Model("./data/patients100.csv");
+
+        ArrayList<ArrayList<String>> oldestPersons = model.findOldest();
+
+        System.out.println(oldestPersons);
+
+
+
+
+//        String testDate = "1979-08-24";
+//        try{
+//            Date testNewDate = new SimpleDateFormat("yyyy-MM-dd").parse(testDate);
+//            Date today = new Date();
+//
+//            long milliSecondsAlive = today.getTime() - testNewDate.getTime();
+//            long diff = TimeUnit.MINUTES.convert(milliSecondsAlive, TimeUnit.MILLISECONDS);
+//
+//            System.out.println(diff);
+//
+//        }catch (Exception e){
+//            System.out.println("somethign went wrong");
+//        }
 
     }
 }
--- Coursework2/src/Model.java
+++ Coursework2/src/Model.java
@@ -1,4 +1,7 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Model {
 
@@ -45,6 +48,60 @@ public class Model {
             }
         }
         return output;
+    }
+
+    public ArrayList<ArrayList<String>> findOldest(){
+        Column birthColumn = this.dataFrame.getColumn("BIRTHDATE");
+        Column deathColumn = this.dataFrame.getColumn("DEATHDATE");
+        ArrayList<ArrayList<String>> oldestPersons = new ArrayList<>();
+        long oldestAge = -1;
+        for (int i = 0; i < dataFrame.getRowCount(); i++) {
+
+            if (deathColumn.getRowValue(i) != ""){
+                continue;
+            }
+
+            if (oldestAge == -1){
+                oldestAge = differenceDate(birthColumn.getRowValue(i));
+                oldestPersons.add(this.dataFrame.getRow(i));
+                continue;
+            }
+
+            long difference = differenceDate(birthColumn.getRowValue(i));
+
+            if (difference > oldestAge){
+                oldestPersons = new ArrayList<>();
+                oldestPersons.add(this.dataFrame.getRow(i));
+                oldestAge = difference;
+            } else if(difference == oldestAge){
+                oldestPersons.add(this.dataFrame.getRow(i));
+            }
+        }
+
+        return oldestPersons;
+
+    }
+
+    private long differenceDate(String date){
+        String today = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
+        return differenceDate(today, date);
+
+    }
+
+    private long differenceDate(String firstDate, String lastDate){
+        long diff = -1;
+        try{
+            Date laterDate = new SimpleDateFormat("yyyy-MM-dd").parse(firstDate);
+            Date earlierDate = new SimpleDateFormat("yyyy-MM-dd").parse(lastDate);
+
+            long milliSecondsAlive = laterDate.getTime() - earlierDate.getTime();
+            diff = TimeUnit.MINUTES.convert(milliSecondsAlive, TimeUnit.MILLISECONDS);
+
+        }catch (Exception e){
+            System.out.println("somethign went wrong");
+        }
+        return diff;
     }
 
 }
