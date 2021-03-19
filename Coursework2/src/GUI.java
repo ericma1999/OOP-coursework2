0529540eae459cacdce8855c2c1b7096aacee540
Eric Ma <officialpassawis@gmail.com>
2021-03-19 23:55:07 +0700

    cleanup imports

--- Coursework2/src/GUI.java
+++ Coursework2/src/GUI.java
@@ -1,9 +1,25 @@
 import uk.ac.ucl.passawis.ui.MySearchDialog;
 import uk.ac.ucl.passawis.ui.MyTable;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JOptionPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 
-import java.awt.*;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Font;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.MouseAdapter;
@@ -14,7 +30,7 @@ import java.util.HashMap;
 public class GUI extends JFrame {
 
     private int page = 0;
-    private Controller controller = new Controller();
+    private final Controller controller = new Controller();
     private final Color sidePanelColour = new Color(61, 105, 240);
     private JButton sidePanelSearchButton;
     private JButton sidePanelDashboardButton;
@@ -23,7 +39,6 @@ public class GUI extends JFrame {
     private JButton writeJSONButton;
     private JPanel sideButtonContainer;
     private JPanel rightPanel;
-    private JPanel sidePanel;
     private JPanel currentFilterContainer;
     private MySearchDialog currentSearchDialog;
     private MyTable table;
@@ -49,6 +64,84 @@ public class GUI extends JFrame {
         createRightPanel();
     }
 
+    private void createSidePanel() {
+        JPanel sidePanel = new JPanel();
+        sidePanelButtonContainer();
+        sidePanel.setBackground(sidePanelColour);
+        sidePanel.add(this.sideButtonContainer, SwingConstants.CENTER);
+        JScrollPane scrollContainer = new JScrollPane(sidePanel,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollContainer.getViewport().setPreferredSize(new Dimension(300, 720));
+        add(scrollContainer, BorderLayout.WEST);
+
+    }
+
+    private void sidePanelButtonContainer() {
+        JPanel buttonContainer = new JPanel(new GridLayout(0, 1, 0, 10));
+        buttonContainer.setMaximumSize(new Dimension(300, 500));
+        buttonContainer.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0));
+        buttonContainer.setBackground(sidePanelColour);
+        createSidePanelSearchButton();
+        createSidePanelDashboardButton();
+        buttonContainer.add(sidePanelDashboardButton, SwingConstants.CENTER);
+        buttonContainer.add(sidePanelSearchButton, SwingConstants.CENTER);
+        this.sideButtonContainer = buttonContainer;
+    }
+
+    private JButton createSidePanelButton(String text) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.BOLD, 18));
+        button.setForeground(Color.white);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setContentAreaFilled(false);
+        return button;
+    }
+
+    private void createSidePanelDashboardButton() {
+        sidePanelDashboardButton = createSidePanelButton("Dashboard");
+        sidePanelDashboardButton.addActionListener((ActionEvent e) -> updateSidePanel(2));
+    }
+
+    private void createErrorDialog(String message){
+        JOptionPane.showMessageDialog(this,
+                message,
+                "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    private void createSidePanelSearchButton() {
+        sidePanelSearchButton = createSidePanelButton("Search");
+        sidePanelSearchButton.addActionListener((ActionEvent e) -> {
+            if (this.table != null) {
+                updateSidePanel(1);
+            } else {
+                createErrorDialog("Please load a file before trying to search");
+            }
+        });
+    }
+
+    private void createRightPanel() {
+        JPanel rightPanel = new JPanel(new GridBagLayout());
+        JButton loadFileButton = new JButton("Load Data");
+        loadFileButton.addActionListener(e -> handleLoadFile());
+        rightPanel.add(loadFileButton);
+        this.rightPanel = rightPanel;
+        add(rightPanel);
+
+    }
+
+    private void handleLoadFile() {
+        String path = showFileDialog();
+        if (path != null && controller.loadFile(path)){
+            loadFileSuccess();
+        }else {
+            createErrorDialog("Failed to read the file selected. Please try again");
+        }
+        this.getContentPane().revalidate();
+        this.getContentPane().repaint();
+    }
+
     private String showFileDialog() {
         JFileChooser fc = new JFileChooser();
         fc.setCurrentDirectory(new File("./"));
@@ -58,18 +151,6 @@ public class GUI extends JFrame {
             return file.getPath();
         }
         return null;
-    }
-
-    private void createSidePanelLoadButton(){
-        this.sidePanelLoadButton = createSidePanelButton("Load New File");
-        this.sidePanelLoadButton.addActionListener(e -> handleLoadFile());
-        sideButtonContainer.add(this.sidePanelLoadButton);
-    }
-
-    private void createSidePanelWriteJSONButton(){
-        this.writeJSONButton = createSidePanelButton("Write to JSON file");
-        this.writeJSONButton.addActionListener(e -> writeJSONFile());
-        sideButtonContainer.add(this.writeJSONButton);
     }
 
     private void writeJSONFile(){
@@ -84,13 +165,6 @@ public class GUI extends JFrame {
     }
 
 
-
-    private void createErrorDialog(String message){
-        JOptionPane.showMessageDialog(this,
-                message,
-                "Error", JOptionPane.ERROR_MESSAGE);
-    }
-
     private void loadFileSuccess(){
         rightPanel.removeAll();
         rightPanel.setLayout(new GridLayout(0, 1));
@@ -103,15 +177,16 @@ public class GUI extends JFrame {
         }
     }
 
-    private void handleLoadFile() {
-        String path = showFileDialog();
-        if (path != null && controller.loadFile(path)){
-            loadFileSuccess();
-        }else {
-            createErrorDialog("Failed to read the file selected. Please try again");
-        }
-        this.getContentPane().revalidate();
-        this.getContentPane().repaint();
+    private void createSidePanelLoadButton(){
+        this.sidePanelLoadButton = createSidePanelButton("Load New File");
+        this.sidePanelLoadButton.addActionListener(e -> handleLoadFile());
+        sideButtonContainer.add(this.sidePanelLoadButton);
+    }
+
+    private void createSidePanelWriteJSONButton(){
+        this.writeJSONButton = createSidePanelButton("Write to JSON file");
+        this.writeJSONButton.addActionListener(e -> writeJSONFile());
+        sideButtonContainer.add(this.writeJSONButton);
     }
 
     private void renderTable() {
@@ -155,68 +230,6 @@ public class GUI extends JFrame {
         add(rightPanel);
         this.table = table;
 
-    }
-
-    private void createRightPanel() {
-        JPanel rightPanel = new JPanel(new GridBagLayout());
-        JButton loadFileButton = new JButton("Load Data");
-        loadFileButton.addActionListener(e -> handleLoadFile());
-        rightPanel.add(loadFileButton);
-        this.rightPanel = rightPanel;
-        add(rightPanel);
-
-    }
-
-    private void createSidePanel() {
-        JPanel sidePanel = new JPanel();
-        sidePanelButtonContainer();
-        sidePanel.setBackground(sidePanelColour);
-        sidePanel.add(this.sideButtonContainer, SwingConstants.CENTER);
-        this.sidePanel = sidePanel;
-        JScrollPane scrollContainer = new JScrollPane(sidePanel,
-                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
-                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollContainer.getViewport().setPreferredSize(new Dimension(300, 720));
-        add(scrollContainer, BorderLayout.WEST);
-
-    }
-
-    private void sidePanelButtonContainer() {
-        JPanel buttonContainer = new JPanel(new GridLayout(0, 1, 0, 10));
-        buttonContainer.setMaximumSize(new Dimension(300, 500));
-        buttonContainer.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0));
-        buttonContainer.setBackground(sidePanelColour);
-        createSidePanelSearchButton();
-        createSidePanelDashboardButton();
-        buttonContainer.add(sidePanelDashboardButton, SwingConstants.CENTER);
-        buttonContainer.add(sidePanelSearchButton, SwingConstants.CENTER);
-        this.sideButtonContainer = buttonContainer;
-    }
-
-    private JButton createSidePanelButton(String text) {
-        JButton button = new JButton(text);
-        button.setFont(new Font("Arial", Font.BOLD, 18));
-        button.setForeground(Color.white);
-        button.setBorderPainted(false);
-        button.setFocusPainted(false);
-        button.setContentAreaFilled(false);
-        return button;
-    }
-
-    private void createSidePanelSearchButton() {
-        sidePanelSearchButton = createSidePanelButton("Search");
-        sidePanelSearchButton.addActionListener((ActionEvent e) -> {
-            if (this.table != null) {
-                updateSidePanel(1);
-            } else {
-                createErrorDialog("Please load a file before trying to search");
-            }
-        });
-    }
-
-    private void createSidePanelDashboardButton() {
-        sidePanelDashboardButton = createSidePanelButton("Dashboard");
-        sidePanelDashboardButton.addActionListener((ActionEvent e) -> updateSidePanel(2));
     }
 
     private void createSidePanelCloseButton() {
