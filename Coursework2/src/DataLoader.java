db5b9a94ebe0f6779f56b79d003e519ad780de13
Eric Ma <officialpassawis@gmail.com>
2021-03-23 14:14:38 +0700

    cleanup data loader

--- Coursework2/src/DataLoader.java
+++ Coursework2/src/DataLoader.java
@@ -9,7 +9,7 @@ public class DataLoader {
     private DataFrame dataFrame = new DataFrame();
     private static final ArrayList<String> supportedTypes = new ArrayList<>(Arrays.asList("json", "csv"));
 
-    public DataLoader(String filePath) throws IOException{
+    public DataLoader(String filePath) throws IOException {
         this.loadContent(filePath);
     }
 
@@ -18,38 +18,43 @@ public class DataLoader {
     }
 
 
-    private String getFileExtension(String path){
+    private String getFileExtension(String path) {
 //        get the position of last . until end of string
-        return path.substring(path.lastIndexOf(".")+1);
+        return path.substring(path.lastIndexOf(".") + 1);
 
     }
 
-    public void loadContent(String filePath) throws IOException{
+    public void loadContent(String filePath) throws IOException {
 
         String extension = this.getFileExtension(filePath);
 
-        if (!supportedTypes.contains(extension)){
-           throw new IOException("File format is incorrect");
+        if (!supportedTypes.contains(extension)) {
+            throw new IOException("File format is incorrect");
         }
 
-        if (extension.equals("json")){
+        if (extension.equals("json")) {
             this.readJSON(filePath);
-        }else{
+        } else {
             FileReader file = new FileReader(filePath);
             this.readCSV(file);
         }
 
     }
 
-    private void readJSON(String filePath) throws IOException{
+    private void readJSON(String filePath) throws IOException {
         JSONReader reader = new JSONReader(filePath);
 
         this.dataFrame = reader.getDataFrame();
     }
 
-    private void readCSV(FileReader file) throws IOException{
 
-            BufferedReader contents = new BufferedReader(file);
+    private void readCSV(FileReader file) throws IOException {
+        /*
+         * try with resources will perform all the cleanup, but since we want to handle
+         * exception at application level allow the exception to propagate up
+         */
+
+        try (BufferedReader contents = new BufferedReader(file)) {
             String currentLine;
             boolean first = true;
             int noOfColumns = -1;
@@ -68,6 +73,7 @@ public class DataLoader {
 
                 this.dataFrame.addRow(currentLineSplitted);
             }
-            contents.close();
+        }
+
     }
 }
